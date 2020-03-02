2f4b07ac12097a78554bbd44701bdd491567713d
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-02 18:58:40 +0000

    Automatically adding a staff user

--- src/main/java/com/ed/webapp/controller/StaffController.java
+++ src/main/java/com/ed/webapp/controller/StaffController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
@@ -20,20 +19,5 @@ public class StaffController {
     @ResponseBody
     public List<Staff> getStaff() {
         return repository.findAll();
-    }
-
-    @PostConstruct
-    public void initialize() {
-        List<Staff> staff = repository.findAll();
-        if (staff.isEmpty()) {
-            // TODO: Move default staff members to txt file
-            Staff s = new Staff("tName", "pass", "temp", "name", 'f');
-            System.out.println(s);
-            try {
-                repository.save(s);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO staff (stf_password, stf_name, stf_surname, stf_username, stf_sex) VALUES
+  ('pass', 'temp', 'name', 'temp', 0);
