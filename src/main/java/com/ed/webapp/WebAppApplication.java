4fe39fdb7ad8fc48f105bf3aefc58c5a638b4a28
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-02 19:30:11 +0000

    Adding default welcome page

--- src/main/java/com/ed/webapp/WebAppApplication.java
+++ src/main/java/com/ed/webapp/WebAppApplication.java
@@ -2,11 +2,25 @@ package com.ed.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
+@Controller
 public class WebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
+    }
+
+    @RequestMapping(value = {"*"})
+    public RedirectView defaultRedirect() {
+        return new RedirectView("index.html");
+    }
+
+    @RequestMapping(value = {"/", "/index.html"})
+    public String frontPage() {
+        return "index";
     }
 }
