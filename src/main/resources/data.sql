b424de3924eb070af8cf82f8ed57d8e741b80a80
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-02 19:27:32 +0000

    Changed way default staff users are added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO staff (stf_password, stf_name, stf_surname, stf_username, stf_sex) VALUES
-  ('pass', 'temp', 'name', 'temp', 0);
+REPLACE INTO staff (stf_id, stf_password, stf_name, stf_surname, stf_username, stf_sex)
+VALUES
+    (1, 'pass', 't', 'a', 'b', 0),
+    (2, 'pass', 'temp', 'name', 'temp', 0);
