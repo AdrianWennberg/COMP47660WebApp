e67086d11b5971079faa99d28f4bc8e8fa0efd7b
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-04 23:36:56 +0000

    Adding default staff data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,20 @@ DELETE FROM module;
 DELETE FROM staff;
 DELETE FROM student;
 
-INSERT INTO staff (stf_id, stf_username, stf_password, stf_name, stf_surname, stf_sex)
+INSERT INTO staff (stf_id, stf_username,stf_password,stf_name,stf_surname,stf_sex)
 VALUES
-    (1, 'temp', 'pass', 'John', 'Doe', 0);
+    (1, 'test', 'pass', 'Test', 'User', 0),
+    (2, 'SEstrada','IAN56NYE7OT','Sonya','Estrada',1),
+    (3, 'CMcclure','WJX98AME2NB','Colin','Mcclure',0),
+    (4, 'DNieves','KAW45WDS1NN','Dillon','Nieves',0),
+    (5, 'ISosa','TRL69RRV4GV','India','Sosa',1),
+    (6, 'CEllis','XKE30ULX5YT','Connor','Ellis',0),
+    (7, 'PWatson','DYK21GYN8LN','Pascale','Watson',0),
+    (8, 'MPruitt','SMT89XOH5TN','MacKensie','Pruitt',1),
+    (9, 'ERogers','QVL96YJC4VW','Erin','Rogers',1),
+    (10, 'XPalmer','DJB13JBQ5EA','Xyla','Palmer',1),
+    (11, 'RHobbs','XOQ14HEO3CK','Reed','Hobbs',0);
+
 
 INSERT INTO module (mdl_id, mdl_maxstd, mdl_name, mdl_topic, stf_id)
 VALUES
