1be292ec1b90a5bff7510dfbe23b5f0ee4757f81
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-04 14:02:16 +0000

    Split staff controller into controller and service

--- src/main/java/com/ed/webapp/controller/StaffController.java
+++ src/main/java/com/ed/webapp/controller/StaffController.java
@@ -1,31 +1,24 @@
 package com.ed.webapp.controller;
 
 import com.ed.webapp.model.Staff;
-import com.ed.webapp.repository.StaffRepository;
+import com.ed.webapp.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StaffController {
 
     @Autowired
-    StaffRepository repository;
-
-    @GetMapping("/staff")
-    @ResponseBody
-    public List<Staff> getStaff() {
-        return repository.findAll();
-    }
+    StaffService service;
 
     @GetMapping("/staff/login")
     public ModelAndView loginPage(ModelMap model, HttpSession session) {
@@ -46,23 +39,13 @@ public class StaffController {
 
     @PostMapping("/staff/login")
     public ModelAndView login(ModelMap model, HttpSession session, @ModelAttribute Staff staff) {
-        List<Staff> found = repository.findByUsername(staff.getStf_username());
-        if (found.size() == 1) {
-            Staff user = found.get(0);
-            if (user.checkPassword(staff.getStf_password())) {
-                session.setAttribute("staff_user", user);
-                if (session.getAttribute("staff_user") != null) {
-                    return new ModelAndView(new RedirectView("/staff/profile"));
-                }
-            }
+
+        Optional<Staff> user = service.getStaffMember(staff);
+        if (user.isEmpty()) {
+            model.addAttribute("login_error", "Incorrect login!");
+            return new ModelAndView("staff/login", model);
         }
-        else if (found.isEmpty()) {
-            System.out.println("Error");
-        }
-        else {
-            throw new RuntimeException("Multiple staff members found!");
-        }
-        model.addAttribute("login_error", "Incorrect login!");
-        return new ModelAndView("staff/login", model);
+        session.setAttribute("staff_user", user.get());
+        return new ModelAndView(new RedirectView("/staff/profile"));
     }
 }
---
+++ src/main/java/com/ed/webapp/service/StaffService.java
@@ -0,0 +1,33 @@
+package com.ed.webapp.service;
+
+import com.ed.webapp.model.Staff;
+import com.ed.webapp.repository.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StaffService {
+
+    @Autowired
+    StaffRepository repository;
+
+    public Optional<Staff> getStaffMember(Staff staff) {
+        List<Staff> foundUsers = repository.findByUsername(staff.getStf_username());
+        if (foundUsers.size() > 1) {
+            throw new DataIntegrityViolationException("Multiple staff members with same username exist: " +
+                                                              staff.getStf_username());
+        }
+        if (foundUsers.isEmpty()) {
+            return Optional.empty();
+        }
+        Staff user = foundUsers.get(0);
+        if (!user.checkPassword(staff.getStf_password())) {
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+}
