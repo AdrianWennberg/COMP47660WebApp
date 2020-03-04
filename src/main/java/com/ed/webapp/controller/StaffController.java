5986c871af6ffa130f7857b06b4a9909ac6a0ccc
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-04 15:47:11 +0000

    Adding modules to staff profile page

--- src/main/java/com/ed/webapp/controller/StaffController.java
+++ src/main/java/com/ed/webapp/controller/StaffController.java
@@ -1,13 +1,12 @@
 package com.ed.webapp.controller;
 
 import com.ed.webapp.model.Staff;
+import com.ed.webapp.service.ModuleService;
 import com.ed.webapp.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -15,35 +14,41 @@ import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/staff")
 public class StaffController {
 
     @Autowired
-    StaffService service;
+    StaffService staffService;
 
-    @GetMapping("/staff/login")
+    @Autowired
+    ModuleService moduleService;
+
+    @GetMapping("/login")
     public ModelAndView loginPage(ModelMap model, HttpSession session) {
         if (session.getAttribute("staff_user") != null) {
             return new ModelAndView(new RedirectView("/"));
         }
         model.addAttribute("staff", new Staff());
-        return new ModelAndView("staff/login", model);
+        return new ModelAndView("/staff/login", model);
     }
 
-    @GetMapping("/staff/profile")
+    @GetMapping("/profile")
     public ModelAndView profilePage(ModelMap model, HttpSession session) {
-        if (session.getAttribute("staff_user") == null) {
+        Staff user = (Staff) session.getAttribute("staff_user");
+        if (user == null) {
             return new ModelAndView(new RedirectView("/staff/login"));
         }
-        return new ModelAndView("staff/profile", model);
+        model.addAttribute("modules", moduleService.getModulesForStaff(user.getStf_ID()));
+        return new ModelAndView("/staff/profile", model);
     }
 
-    @PostMapping("/staff/login")
+    @PostMapping("/login")
     public ModelAndView login(ModelMap model, HttpSession session, @ModelAttribute Staff staff) {
 
-        Optional<Staff> user = service.getStaffMember(staff);
+        Optional<Staff> user = staffService.getStaffMember(staff);
         if (user.isEmpty()) {
             model.addAttribute("login_error", "Incorrect login!");
-            return new ModelAndView("staff/login", model);
+            return new ModelAndView("/staff/login", model);
         }
         session.setAttribute("staff_user", user.get());
         return new ModelAndView(new RedirectView("/staff/profile"));
