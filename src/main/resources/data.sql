512bf3680444c7c8f7f42555268041f962189ba4
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-03-04 15:06:12 +0000

    Cleaning up temp data and adding student data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
-REPLACE INTO staff (stf_id, stf_password, stf_name, stf_surname, stf_username, stf_sex)
+DELETE FROM module;
+DELETE FROM staff;
+DELETE FROM student;
+
+INSERT INTO staff (stf_id, stf_password, stf_name, stf_surname, stf_username, stf_sex)
 VALUES
-    (1, 'pass', 't', 'a', 'b', 0),
-    (2, 'pass', 'temp', 'name', 'temp', 0);
+    (1, 'pass', 'temp', 'John', 'Dow', 0);
+
+INSERT INTO module (mdl_id, mdl_maxstd, mdl_name, mdl_topic, stf_id)
+VALUES
+    (1, 15, 'Temp Module', 'Science', 1);
+
+INSERT INTO student (std_username, std_id, std_address, std_email, std_name, std_password , std_sex, std_surname)
+VALUES
+    ('temp', 1, 'addr', 'temp@example.com', 'Joe', 'pass', 1, 'D');
