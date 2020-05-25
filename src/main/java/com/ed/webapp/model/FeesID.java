b082cc6ec8b6835f5d8aeca7ce956c52d4e36342
Adrian Wennberg <adrian.wennberg@ucdconnect.ie>
2020-05-25 22:40:39 +0200

    Add equals and hashcode to FeesID

--- src/main/java/com/ed/webapp/model/FeesID.java
+++ src/main/java/com/ed/webapp/model/FeesID.java
@@ -1,8 +1,26 @@
 package com.ed.webapp.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class FeesID implements Serializable {
     Long fee_student;
     int fee_year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FeesID feesID = (FeesID) o;
+        return fee_year == feesID.fee_year && fee_student.equals(feesID.fee_student);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fee_student, fee_year);
+    }
 }
